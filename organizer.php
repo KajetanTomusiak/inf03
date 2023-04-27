e65e98fa526654e38d103594e04631741df30844
KajetanTomusiak <120165385+KajetanTomusiak@users.noreply.github.com>
2023-04-27 11:41:11 +0200

    Add files via upload

---
+++ organizer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sierpniowy kalendarz</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+<body>
+    <header>
+        <div class="banner1">
+            <p>Organizer: SIERPIEŃ</p>
+        </div>
+        <div class="banner2">
+            <form action="" method="post">
+
+            </form>
+        </div>
+        <div class="banner3">
+            <img src="logo2.png" alt="sierpień">
+        </div>
+    </header>
+    <main>
+        <?php
+
+        ?>
+    </main>
+    <footer>
+        <p>Stronę wykonał: Kajetan Tomusiak</p>
+    </footer>
+    
+</body>
+</html>
